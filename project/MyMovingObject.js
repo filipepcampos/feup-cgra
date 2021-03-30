c93ba97bf5941936f47e8bd31943dcd86ee5e9e6
Filipe Campos <up201905609@fe.up.pt>
2021-03-30 17:55:52 +0100

    Complete Project Task 1

---
+++ project/MyMovingObject.js
@@ -0,0 +1,37 @@
+import { CGFobject } from "../lib/CGF.js";
+import { MyPyramid } from "./MyPyramid.js";
+
+export class MyMovingObject extends CGFobject{
+    constructor(scene){
+        super(scene);
+        this.reset();
+        this.pyramid = new MyPyramid(scene, 10, 10);
+    }
+
+    update() {
+        this.position[0] += Math.sin(this.orientation) * this.speed;
+        this.position[2] += Math.cos(this.orientation) * this.speed;
+    }
+
+    turn(val) {
+        this.orientation += val;
+    }
+    
+    accelerate(val){
+        this.speed += val;
+        this.speed = Math.max(this.speed, 0);
+    }
+
+    reset(){
+        this.speed = 0;
+        this.position = [0, 0, -0.5];
+        this.orientation = 0.0;
+    }
+
+    display(){
+        this.scene.translate(...this.position);
+        this.scene.rotate(this.orientation, 0, 1, 0);
+        this.scene.rotate(Math.PI/2, 1, 0, 0);
+        this.pyramid.display();
+    }
+}
