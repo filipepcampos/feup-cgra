5e8f967cfa89ff419df0d3d499466f264a2d1019
Filipe Campos <filipepcampos24@gmail.com>
2021-05-07 23:07:54 +0100

    Bug fix :hammer:, MyRotatedPyramid wasn't being displayed

--- project/MyRotatedPyramid.js
+++ project/MyRotatedPyramid.js
@@ -4,6 +4,7 @@ export class MyRotatedPyramid extends MyPyramid{
     display(){
         this.scene.pushMatrix();
         this.scene.rotate(Math.PI/2, 1, 0, 0);
+        super.display();
         this.scene.popMatrix();
     }
 }
